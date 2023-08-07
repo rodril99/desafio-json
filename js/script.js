b747ea2704443a86512b61b92c1ce452be4aea4c
Rodrigo Ramos <rodryger1@gmail.com>
2023-08-07 19:46:09 -0300

    estudiantes

--- js/script.js
+++ js/script.js
@@ -5,11 +5,20 @@ const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mo
 
 const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
 
+
+
+fetch (DATA_URL)
+  .then((response) =>  response.json())
+  .then((data) => {
+    showData(data.students);
+  });
+
 /**
  * Función que recibe por parámetro un array con los datos que se mostrarán en el DOM
  * Los datos se mostrarán dentro del div de id "container" y por cada ítem se está creando un nuevo párrafo donde se
  * imprime el campo "name" y el campo "lastname" separados por un espacio
  */
+
 function showData(dataArray) {
   // El for itera sobre los elementos del array
   for (const item of dataArray) {
